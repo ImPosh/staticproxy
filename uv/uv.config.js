484fa1ea8019c3c5f03bc3281c505a4e6fc5346c
GrinLOLZ <149744857+Xenapsis@users.noreply.github.com>
2024-01-16 21:25:13 -0500

    stuff

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://sunshine.sun.2of1.org/v/',
+    bare: '',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
