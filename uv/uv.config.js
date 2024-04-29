1f875ab34ee109342eb328a7c1844df6ab999032
posh <101807149+ImPosh@users.noreply.github.com>
2024-04-29 12:52:36 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://1f42b217-bdc3-4ebf-b692-d93ce68d512e-00-1vdpfgr0g4tm9.worf.replit.dev/bare/',
+    bare: 'https://juniorhighmaths.tafca.co.uk/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
